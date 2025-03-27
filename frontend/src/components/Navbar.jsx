16eec491be2a080ba36d105c05c6073bdc00748d
leighTOR <leighinna.jamolin@lspu.edu.ph>
2025-03-27 10:34:15 +0800

    redesign navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { Link } from 'react-router-dom'
+import { Link as ScrollLink } from 'react-scroll'
 import logo from '../assets/logo.png'
 
 const Navbar = () => {
@@ -10,46 +10,91 @@ const Navbar = () => {
   }
 
   return (
-    <nav className="bg-white shadow-sm sticky top-0 z-10">
-      <div className="container mx-auto px-4 py-3 flex items-center justify-between">
-        {/* Left: Logo or Brand */}
+    <nav className="bg-[#F9FAFF] shadow-sm sticky top-0 z-10">
+      <div className="container mx-auto px-4 py-5 flex items-center justify-between">
+        {/* Left: Logo + Brand */}
         <div className="flex items-center">
-          <img src={logo} alt="IhuruCyber Logo" className="h-8 w-8 mr-2" />
-          <Link to="/" className="text-xl font-semibold text-gray-800">
+          <img src={logo} alt="RivanCyber Logo" className="h-8 w-8 mr-2" />
+          <ScrollLink 
+            to="home" 
+            smooth={true} 
+            duration={500} 
+            offset={-70} 
+            className="text-2xl font-semibold text-[#0D2153] cursor-pointer"
+          >
             RivanCyber
-          </Link>
+          </ScrollLink>
         </div>
 
         {/* Middle: Nav Links (hidden on mobile) */}
         <div className="hidden md:flex space-x-6">
-          <Link to="/" className="text-gray-600 hover:text-gray-900">
+          <ScrollLink 
+            to="home" 
+            smooth={true} 
+            duration={500} 
+            offset={-70} 
+            className="text-gray-600 hover:text-gray-900 cursor-pointer"
+          >
             Home
-          </Link>
-          <Link to="/about" className="text-gray-600 hover:text-gray-900">
+          </ScrollLink>
+          <ScrollLink 
+            to="about" 
+            smooth={true} 
+            duration={500} 
+            offset={-70} 
+            className="text-gray-600 hover:text-gray-900 cursor-pointer"
+          >
             About
-          </Link>
-          <Link to="/services" className="text-gray-600 hover:text-gray-900">
+          </ScrollLink>
+          <ScrollLink 
+            to="services" 
+            smooth={true} 
+            duration={500} 
+            offset={-70} 
+            className="text-gray-600 hover:text-gray-900 cursor-pointer"
+          >
             Services
-          </Link>
-          <Link to="/courses" className="text-gray-600 hover:text-gray-900">
+          </ScrollLink>
+          <ScrollLink 
+            to="courses" 
+            smooth={true} 
+            duration={500} 
+            offset={-70} 
+            className="text-gray-600 hover:text-gray-900 cursor-pointer"
+          >
             Courses
-          </Link>
-          <Link to="/reviews" className="text-gray-600 hover:text-gray-900">
+          </ScrollLink>
+          <ScrollLink 
+            to="reviews" 
+            smooth={true} 
+            duration={500} 
+            offset={-70} 
+            className="text-gray-600 hover:text-gray-900 cursor-pointer"
+          >
             Reviews
-          </Link>
-          <Link to="/contact" className="text-gray-600 hover:text-gray-900">
+          </ScrollLink>
+          <ScrollLink 
+            to="contact" 
+            smooth={true} 
+            duration={500} 
+            offset={-70} 
+            className="text-gray-600 hover:text-gray-900 cursor-pointer"
+          >
             Contact
-          </Link>
+          </ScrollLink>
         </div>
 
         {/* Right: "Request a Quote" button (hidden on mobile) */}
         <div className="hidden md:block">
-          <Link
-            to="/quotation"
-            className="px-5 py-2 bg-blue-900 text-white rounded-full hover:bg-blue-800"
+          <ScrollLink 
+            to="quotation" 
+            smooth={true} 
+            duration={500} 
+            offset={-70} 
+            className="px-5 py-2 bg-[#0D2153] text-white rounded-full hover:bg-[#0B1C47] cursor-pointer"
           >
             Request a Quote
-          </Link>
+          </ScrollLink>
         </div>
 
         {/* Mobile: Hamburger Icon */}
@@ -58,19 +103,11 @@ const Navbar = () => {
           className="md:hidden text-gray-600 hover:text-gray-900 focus:outline-none"
         >
           {isOpen ? (
-            /* Close (X) icon */
-            <svg
-              className="h-6 w-6 fill-current"
-              viewBox="0 0 24 24"
-            >
+            <svg className="h-6 w-6 fill-current" viewBox="0 0 24 24">
               <path d="M6 6L18 18M6 18L18 6" />
             </svg>
           ) : (
-            /* Hamburger icon */
-            <svg
-              className="h-6 w-6 fill-current"
-              viewBox="0 0 24 24"
-            >
+            <svg className="h-6 w-6 fill-current" viewBox="0 0 24 24">
               <path d="M4 5h16v2H4V5zm0 6h16v2H4v-2zm0 6h16v2H4v-2z" />
             </svg>
           )}
@@ -79,58 +116,79 @@ const Navbar = () => {
 
       {/* Mobile Menu (slide-down) */}
       {isOpen && (
-        <div className="md:hidden bg-white shadow-sm">
-          <div className="px-4 pt-2 pb-3 space-y-2">
-            <Link
-              to="/"
-              className="block text-gray-600 hover:text-gray-900"
-              onClick={() => setIsOpen(false)}
+        <div className="md:hidden bg-[#F9FAFF] shadow-sm">
+          <div className="px-4 pt-3 pb-4 space-y-2">
+            <ScrollLink 
+              onClick={() => setIsOpen(false)} 
+              to="home" 
+              smooth={true} 
+              duration={500} 
+              offset={-70} 
+              className="block text-gray-600 hover:text-gray-900 cursor-pointer"
             >
               Home
-            </Link>
-            <Link
-              to="/about"
-              className="block text-gray-600 hover:text-gray-900"
-              onClick={() => setIsOpen(false)}
+            </ScrollLink>
+            <ScrollLink 
+              onClick={() => setIsOpen(false)} 
+              to="about" 
+              smooth={true} 
+              duration={500} 
+              offset={-70} 
+              className="block text-gray-600 hover:text-gray-900 cursor-pointer"
             >
               About
-            </Link>
-            <Link
-              to="/services"
-              className="block text-gray-600 hover:text-gray-900"
-              onClick={() => setIsOpen(false)}
+            </ScrollLink>
+            <ScrollLink 
+              onClick={() => setIsOpen(false)} 
+              to="services" 
+              smooth={true} 
+              duration={500} 
+              offset={-70} 
+              className="block text-gray-600 hover:text-gray-900 cursor-pointer"
             >
               Services
-            </Link>
-            <Link
-              to="/courses"
-              className="block text-gray-600 hover:text-gray-900"
-              onClick={() => setIsOpen(false)}
+            </ScrollLink>
+            <ScrollLink 
+              onClick={() => setIsOpen(false)} 
+              to="courses" 
+              smooth={true} 
+              duration={500} 
+              offset={-70} 
+              className="block text-gray-600 hover:text-gray-900 cursor-pointer"
             >
               Courses
-            </Link>
-            <Link
-              to="/reviews"
-              className="block text-gray-600 hover:text-gray-900"
-              onClick={() => setIsOpen(false)}
+            </ScrollLink>
+            <ScrollLink 
+              onClick={() => setIsOpen(false)} 
+              to="reviews" 
+              smooth={true} 
+              duration={500} 
+              offset={-70} 
+              className="block text-gray-600 hover:text-gray-900 cursor-pointer"
             >
               Reviews
-            </Link>
-            <Link
-              to="/contact"
-              className="block text-gray-600 hover:text-gray-900"
-              onClick={() => setIsOpen(false)}
+            </ScrollLink>
+            <ScrollLink 
+              onClick={() => setIsOpen(false)} 
+              to="contact" 
+              smooth={true} 
+              duration={500} 
+              offset={-70} 
+              className="block text-gray-600 hover:text-gray-900 cursor-pointer"
             >
               Contact
-            </Link>
+            </ScrollLink>
             {/* Mobile "Request a Quote" button */}
-            <Link
-              to="/quotation"
-              className="block text-center mt-2 px-5 py-2 bg-blue-900 text-white rounded-full hover:bg-blue-800"
-              onClick={() => setIsOpen(false)}
+            <ScrollLink 
+              onClick={() => setIsOpen(false)} 
+              to="quotation" 
+              smooth={true} 
+              duration={500} 
+              offset={-70} 
+              className="block text-center mt-2 px-5 py-2 bg-[#0D2153] text-white rounded-full hover:bg-[#0B1C47] cursor-pointer"
             >
               Request a Quote
-            </Link>
+            </ScrollLink>
           </div>
         </div>
       )}
